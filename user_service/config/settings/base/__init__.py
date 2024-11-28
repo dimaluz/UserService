2c1acba476539333b7ad50dedaa0e6f228d3a592
dimaluz <dimaluz@rambler.ru>
2024-11-28 21:21:19 +0300

    drf settings was added

--- user_service/config/settings/base/__init__.py
+++ user_service/config/settings/base/__init__.py
@@ -2,4 +2,5 @@ from .apps import *
 from .auth import *
 from .basic import *
 from .database import *
+from .drf import *
 from .logging import *
--- user_service/config/settings/base/apps.py
+++ user_service/config/settings/base/apps.py
@@ -7,4 +7,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third party apps:
+    "rest_framework",
+    "rest_framework_simplejwt",
 ]
